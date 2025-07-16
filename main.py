db35ec7857d9331c9b03463b231991db1ca297de
Om Rajput <150363099+OmRajput17@users.noreply.github.com>
2025-07-16 22:41:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.generate_gpt2 import generate_text
 
 if __name__ == "__main__":
-    prompt = "As the carriage rolled through the rain, Elizabeth looked at Darcy and said"
+    prompt = "As the carriage rolled through the rain, Elizabeth looked at Darcy and said that"
     output = generate_text(prompt)
     print("\n Generated Text:\n")
     print(output)
